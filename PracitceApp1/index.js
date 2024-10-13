6cd34a7549f24b4dc47e167f2a4174e53cdac22a
DevEbrahim1 <iamebrahimkholil@gmail.com>
2024-10-13 16:24:00 +0600

    Update index.js

--- PracitceApp1/index.js
+++ PracitceApp1/index.js
@@ -3,7 +3,17 @@ var express=require('express');
 app=express();
 
 app.get("/",function(req,res){
-    res.send("Hello Express Js");
+    res.send("Home Page");
+});
+app.post('/about', function(req,res){
+    res.send("About Page")
+
+});
+app.put('/contact',function(req,res){
+    res.send("Contact page")
+});
+app.delete("/terms",function(req,res){
+    res.send("Terms-page")
 });
 app.listen(8080,function(){
     console.log('Server Run Success')
